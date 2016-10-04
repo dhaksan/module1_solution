6ba6129f8f310e91205bab4521dcc8868c74b792
dhaksan <andydhakal@gmail.com>
2016-10-04 19:06:28 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # fullstack-coursera
 Repository for the practice
-"# module1_solution" 
---
+++ app.js
@@ -0,0 +1,35 @@
+(function (){
+            'use strict';
+            angular.module('foodcheck', [])
+            .controller('lunchCheckController', ['$scope', '$filter', lunchCheckController]);
+
+            function lunchCheckController($scope, $filter){
+                $scope.name="";
+                $scope.lengthValue=0;
+
+                $scope.lengthcheck=function () {
+                  var totalNameValue=calculatenumericvalue($scope.name);
+                  $scope.lengthValue=totalNameValue;
+                };
+
+                function calculatenumericvalue(string){
+                      $scope.array = $scope.name.split(', ');
+                      var totalStringValue=($scope.array.length);
+                      return(totalStringValue);
+
+                };
+
+
+                  $scope.evallength=function(){
+                    if ($scope.lengthValue >=1 && $scope.lengthValue <=3){
+                      $scope.lunch="Enjoy Your Lunch!";
+                    }
+                    else if ($scope.lengthValue > 3 ){
+                      $scope.lunch="Too much!!";
+                    }
+                    else {
+                      $scope.lunch="Please enter something first";
+                    }
+                  };
+};
+})();
